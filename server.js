a732479c25d086b9b2de93ec30d31c990351465d
Brandon Farmer <bthesorceror@gmail.com>
2017-06-25 19:40:01 -0600

    Add user to socket on connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
@@ -5,17 +6,20 @@ const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser')
 const passport = require('./passport')
+const User = require('./app/models/user')
 
 let app = express()
 
 app.use('/public', express.static('public'))
 
-app.use(session({
+let sessions = session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   cookie: { secure: 'auto' }
-}))
+})
+
+app.use(sessions)
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -35,8 +39,26 @@ app.get('/app', (req, res) => {
 let server = http.Server(app)
 let io = socketio(server)
 
+io.use((socket, next) => {
+  sessions(socket.request, socket.request.res, next)
+})
+
+io.use((socket, next) => {
+  let id = _.get(socket.request.session, 'passport.user')
+
+  if (!id) {
+    return next()
+  }
+
+  User.findById(id).then(function (user) {
+    socket.user = user
+    next()
+  }).catch(next)
+})
+
 io.on('connection', (socket) => {
   console.log('Connected')
+  console.dir(socket.user)
 })
 
 module.exports = server
