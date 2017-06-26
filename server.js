40fef2aa59237407c7b9c72900f0a253f3afe8d0
Brandon Farmer <bthesorceror@gmail.com>
2017-06-25 19:31:08 -0600

    Setup socket.io

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+const http = require('http')
 const path = require('path')
+const socketio = require('socket.io')
 const express = require('express')
 const session = require('express-session')
 const bodyParser = require('body-parser')
 const passport = require('./passport')
 
-let app = module.exports = express()
+let app = express()
+
+app.use('/public', express.static('public'))
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -23,3 +27,16 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+app.get('/app', (req, res) => {
+  res.render('app')
+})
+
+let server = http.Server(app)
+let io = socketio(server)
+
+io.on('connection', (socket) => {
+  console.log('Connected')
+})
+
+module.exports = server
