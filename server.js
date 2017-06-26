6b05997c74e9845a3a8fecd13b1a9b85983625ea
Brandon Farmer <bthesorceror@gmail.com>
2017-06-26 00:55:42 -0600

    Add messages create endpoint

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use('/public', express.static('public'))
+app.use('/api', bodyParser.json())
+app.use('/api', requireUser)
 app.use(sessions)
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -42,7 +44,7 @@ app.get('/app', requireUser, (req, res) => {
   res.render('app')
 })
 
-app.get('/api/usernames', requireUser, (req, res) => {
+app.get('/api/usernames', (req, res) => {
   User.findAllUsernames().then((usernames) => {
     res.json({ usernames: usernames })
   }).catch(() => {
@@ -50,10 +52,42 @@ app.get('/api/usernames', requireUser, (req, res) => {
   })
 })
 
-app.get('/api/messages', requireUser, (req, res) => {
+app.get('/api/messages', (req, res) => {
   Message.findAllByUser(req.user).then((messages) => {
     let grouped = _.groupBy(messages, 'from')
     res.json({ messages: grouped })
+  }).catch(() => {
+    res.status(500).json({ error: 'An error has occurred' })
+  })
+})
+
+app.post('/api/messages', (req, res) => {
+  let receipent
+  let to = req.body.to
+  let text = req.body.text
+
+  if (!to) {
+    return res.status(400).json({
+      error: 'No receipent given'
+    })
+  }
+
+  if (!text) {
+    return res.status(400).json({
+      error: 'No text given'
+    })
+  }
+
+  User.findByUsername(to).then((toUser) => {
+    receipent = toUser
+    return Message.create({
+      to_id: receipent.id,
+      from_id: req.user.id,
+      text: text
+    })
+  }).then(() => {
+    // Dispatch Message
+    res.status(201).end()
   }).catch(() => {
     res.status(500).json({ error: 'An error has occurred' })
   })
